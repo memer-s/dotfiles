930c5e5ae89ff11f97af8758df34e245cf0e6ec3
memer-s <mimeremim@gmail.com>
2021-10-30 20:00:06 +0200

    Hello world

---
+++ ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing nvim..."
+apt install nvim
+
+echo "Installing vim-plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo "Configuring files..."
+mkdir ~/.config/nvim/
+
+cp init.vim ~/.config/nvim/

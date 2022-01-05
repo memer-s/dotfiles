a75ff4afac6984cb7ee3c59deb2df60be7618041
memer-s <mimeremim@gmail.com>
2022-01-05 15:55:15 +0100

    bruhhhhhhhh.,

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Installing nvim..."
-apt install nvim
+echo "--- NVIM INSTALL LOG ---" >> logfile
+apt install nvim >> logfile
 
 echo "Installing vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -10,3 +11,5 @@ echo "Configuring files..."
 mkdir ~/.config/nvim/
 
 cp init.vim ~/.config/nvim/
+cp coc-settings.json ~/.config/nvim/
+cp .tmux.conf ~/
